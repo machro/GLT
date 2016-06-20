a5c06c6487ab807224c79635e11fbf4fd8795e34
M. Craenen <m.craenen@ilysian.com>
2016-06-20 12:31:03 +0200

    Last changes i hope

--- assignment4/a4_platoon/src/nl/tue/glt/validation/PlatoonValidator.xtend
+++ assignment4/a4_platoon/src/nl/tue/glt/validation/PlatoonValidator.xtend
@@ -3,6 +3,11 @@
  */
 package nl.tue.glt.validation
 
+import org.eclipse.xtext.validation.Check
+import nl.tue.glt.platoon.FV
+import nl.tue.glt.platoon.PlatoonPackage
+import org.eclipse.emf.ecore.EObject
+import nl.tue.glt.platoon.Vehicle
 
 /**
  * This class contains custom validation rules. 
@@ -10,7 +15,38 @@ package nl.tue.glt.validation
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#validation
  */
 class PlatoonValidator extends AbstractPlatoonValidator {
+	@Check
+	def checkNotPullingItself(FV fv) {
+		if (fv.name.equals(fv.pulledBy.name)) {
+			error("A vehicle cannot pull itself.", PlatoonPackage.Literals.FV__PULLED_BY);
+		}
+	}
 	
+	@Check
+	def checkSameFrontRunners(FV fv) {
+		for(EObject obj : fv.eContainer.eContents) {
+			if (obj != fv) {
+				if (obj instanceof FV) {
+					if (obj.pulledBy == fv.pulledBy) {
+						error ("A vehicle can only pull one FV.", PlatoonPackage.Literals.FV__PULLED_BY);
+					}
+				}
+			}
+		}
+	}
+	
+	@Check
+	def checkUniqueName(Vehicle vehicle) {
+		for(EObject obj : vehicle.eContainer.eContents) {
+			if (obj != vehicle) {
+				if (obj instanceof Vehicle) {
+					if (obj.name == vehicle.name) {
+						error ("Vehicle names must be unique.", PlatoonPackage.Literals.VEHICLE__NAME);
+					}
+				}
+			}
+		}
+	}
 //	public static val INVALID_NAME = 'invalidName'
 //
 //	@Check
--- assignment4/a4_platoon/xtend-gen/nl/tue/glt/validation/PlatoonValidator.java
+++ assignment4/a4_platoon/xtend-gen/nl/tue/glt/validation/PlatoonValidator.java
@@ -3,7 +3,14 @@
  */
 package nl.tue.glt.validation;
 
+import com.google.common.base.Objects;
+import nl.tue.glt.platoon.FV;
+import nl.tue.glt.platoon.PlatoonPackage;
+import nl.tue.glt.platoon.Vehicle;
 import nl.tue.glt.validation.AbstractPlatoonValidator;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.validation.Check;
 
 /**
  * This class contains custom validation rules.
@@ -12,4 +19,52 @@ import nl.tue.glt.validation.AbstractPlatoonValidator;
  */
 @SuppressWarnings("all")
 public class PlatoonValidator extends AbstractPlatoonValidator {
+  @Check
+  public void checkNotPullingItself(final FV fv) {
+    String _name = fv.getName();
+    Vehicle _pulledBy = fv.getPulledBy();
+    String _name_1 = _pulledBy.getName();
+    boolean _equals = _name.equals(_name_1);
+    if (_equals) {
+      this.error("A vehicle cannot pull itself.", PlatoonPackage.Literals.FV__PULLED_BY);
+    }
+  }
+  
+  @Check
+  public void checkSameFrontRunners(final FV fv) {
+    EObject _eContainer = fv.eContainer();
+    EList<EObject> _eContents = _eContainer.eContents();
+    for (final EObject obj : _eContents) {
+      boolean _notEquals = (!Objects.equal(obj, fv));
+      if (_notEquals) {
+        if ((obj instanceof FV)) {
+          Vehicle _pulledBy = ((FV)obj).getPulledBy();
+          Vehicle _pulledBy_1 = fv.getPulledBy();
+          boolean _equals = Objects.equal(_pulledBy, _pulledBy_1);
+          if (_equals) {
+            this.error("A vehicle can only pull one FV.", PlatoonPackage.Literals.FV__PULLED_BY);
+          }
+        }
+      }
+    }
+  }
+  
+  @Check
+  public void checkUniqueName(final Vehicle vehicle) {
+    EObject _eContainer = vehicle.eContainer();
+    EList<EObject> _eContents = _eContainer.eContents();
+    for (final EObject obj : _eContents) {
+      boolean _notEquals = (!Objects.equal(obj, vehicle));
+      if (_notEquals) {
+        if ((obj instanceof Vehicle)) {
+          String _name = ((Vehicle)obj).getName();
+          String _name_1 = vehicle.getName();
+          boolean _equals = Objects.equal(_name, _name_1);
+          if (_equals) {
+            this.error("Vehicle names must be unique.", PlatoonPackage.Literals.VEHICLE__NAME);
+          }
+        }
+      }
+    }
+  }
 }
